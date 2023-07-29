debe131fa8d4b8c0eef1cdcea3293ed45665ed05
Arthur Libberecht <arthurlibberecht@student.hogent.be>
2023-07-29 18:34:47 +0200

    database server opzetting

---
+++ src/Server/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+using Shared.Infrastructrure;
+using Domain.Exceptions;
+
+namespace Server.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly RequestDelegate next;
+
+    public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, RequestDelegate next)
+    {
+        _logger = logger;
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Something went wrong");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        ErrorDetails error = exception switch
+        {
+            EntityNotFoundException ex => new ErrorDetails(ex.Message, HttpStatusCode.NotFound),
+            EntityAlreadyExistsException ex => new ErrorDetails(ex.Message, HttpStatusCode.Conflict),
+            // Add more custom exceptions here...
+            ApplicationException ex => new ErrorDetails(ex.Message),
+            _ => new ErrorDetails(exception.Message)
+        };
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)error.StatusCode;
+        await context.Response.WriteAsync(error.ToString());
+    }
+}
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,20 +1,88 @@
-using Microsoft.AspNetCore.Hosting;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Persistence.Configuration;
+using Server.Middleware;
+using Services;
 
-namespace Server
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddServices();
+
+// Fluentvalidation
+//builder.Services.AddValidatorsFromAssemblyContaining<>();
+//builder.Services.AddFluentValidationAutoValidation();
+
+// Swagger | OAS 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // Since we subclass our dto's we need a more unique id.
+    options.CustomSchemaIds(type => type.DeclaringType is null ? $"{type.Name}" : $"{type.DeclaringType?.Name}.{type.Name}");
+    //options.
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Database
+builder.Services.AddDbContext<HerExamenDBContext>(options =>
+{
+    options.UseSqlServer
+    (
+        builder.Configuration.GetConnectionString("SqlServer")
+    );
+});
+
+// (Fake) Authentication
+//builder.Services.AddAuthentication("Fake Authentication")
+//                .AddScheme<AuthenticationSchemeOptions, FakeAuthenticationHandler>("Fake Authentication", null);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpContextAccessor>().HttpContext.User);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers().RequireAuthorization();
+app.MapFallbackToFile("index.html");
+
+
+using (var scope = app.Services.CreateScope())
+{ // Require a DbContext from the service provider and seed the database.
+    var dbContext = scope.ServiceProvider.GetRequiredService<HerExamenDBContext>();
+    //FakeSeeder seeder = new(dbContext);
+    //seeder.Seed();
+}
+
+app.Run();
